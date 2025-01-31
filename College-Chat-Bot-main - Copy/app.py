600f4a2a87fd9a78a4885b2fd3f12d60bfa2845e
RITHANYT <rithanyathangaveltn@gmail.com>
2025-01-31 21:29:59 +0530

    Some changes on app.py file

--- College-Chat-Bot-main - Copy/app.py
+++ College-Chat-Bot-main - Copy/app.py
@@ -1,4 +1,4 @@
-"""from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import random
 import json
 import os
@@ -37,4 +37,4 @@ if __name__ == '__main__':
 
 
 
-"""
+
