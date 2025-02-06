926cdb0ce3efcfefbec80ed89905fc8e0e75ca73
RITHANYT <rithanyathangaveltn@gmail.com>
2025-02-06 22:05:38 +0530

    Changes in train.py

--- College-Chat-Bot-main - Copy/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Load the file
-model = np.load('bert_model.npy', allow_pickle=True).item()
-
-# Print available keys in the model
-print(model.keys())  # This will show what keys are stored in the file
